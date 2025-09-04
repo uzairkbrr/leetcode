8b1a013cd76b93026540e2c24f23cf8493eef6e7
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-04 16:49:38 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (6.99%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        """
+        :type x: int
+        :type y: int
+        :type z: int
+        :rtype: int
+        """
+        if abs(x-z) == abs(y-z):
+            return 0
+        elif abs(x-z) < abs(y-z):
+            return 1
+        else:
+            return 2
+        
