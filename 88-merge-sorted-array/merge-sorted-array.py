d0d3153279ec11122c980ed39259fb12613150ff
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-06 14:26:08 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (83.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -8,20 +8,15 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
 
-        # Store nums1 values in n1Copy; To merge the arrays in nums1;
-        n1Copy = nums1[0: m]
-        # p1 for nums1; p2 for nums2;
-        p1 = 0
-        p2 = 0
+        p1 = m-1
+        p2 = n-1
+        i = m+n-1
 
-        # Iterate over the entire length of nums1 (m + n slots)
-        for i in range(m + n):
-            # Decide whether to take from n1Copy or nums2:
-            # Case 1: If nums2 is already exhausted (p2 >= n), we must take from n1Copy.
-            # Case 2: If nums1 still has elements (p1 < m), AND the current element in n1Copy is <= current element in nums2, then take from n1Copy.
-            if p2 >= n or (p1 < m and n1Copy[p1] <= nums2[p2]):
-                nums1[i] = n1Copy[p1]
-                p1 += 1
+        while p2 >= 0:
+            if p1>= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
             else:
                 nums1[i] = nums2[p2]
-                p2 += 1 
+                p2 -= 1
+            i -= 1
