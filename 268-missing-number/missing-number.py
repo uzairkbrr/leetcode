f6e7ea1852668842c0e4db755f8a9994c86d0965
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-08 16:37:32 +0500

    Time: 2571 ms (18.19%) | Memory: 13.4 MB (30.34%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i
+
+        return None
