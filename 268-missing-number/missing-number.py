f0cdd9d2e77f5d87a38007c20476c9b6b3c909bf
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-08 16:41:22 +0500

    Time: 2651 ms (9.60%) | Memory: 13.4 MB (30.34%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -8,3 +8,13 @@ class Solution(object):
         for i in range(n + 1):
             if i not in nums:
                 return i
+
+        n = len(nums)
+        # find sum till n;
+        expected_sum = n * (n + 1) // 2   
+        
+        # Now find the actual sum ( all the values which is in nums )
+        actual_sum = sum(nums)
+
+        # The difference is the target/required value
+        return expected_sum - actual_sum
