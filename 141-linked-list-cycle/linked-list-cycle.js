060b6677df098f437c7b10efa96828ebaa32655f
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-17 18:20:19 +0500

    Time: 44 ms (92.34%) | Memory: 59.1 MB (64.31%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (head == null || head.next == null) {
+        return false;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;         
+        fast = fast.next.next;    
+
+        if (slow === fast) {       
+            return true;
+        }
+    }
+
+    // reached the end → no cycle
+    return false; 
+};
