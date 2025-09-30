386b2353459d74cef286978f78dbb2d898e82734
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-09-30 22:35:25 +0500

    Time: 0 ms (100.00%) | Memory: 55.4 MB (18.59%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -1,15 +1,28 @@
 /**
  * @param {string} jewels
  * @param {string} stones
- * @return {number}
+ * @return {number} 
  */
 var numJewelsInStones = function(jewels, stones) {
-    let count = 0;
+    // Brute force approach
+    // let count = 0;
+    // for (let i=0; i<jewels.length; i++) {
+    //     for (let j=0; j<stones.length; j++) {
+    //         if (jewels[i] == stones[j]) {
+    //             count++;
+    //         }
+    //     }
+    // }
+
+    let jewelSet = new Set();
     for (let i=0; i<jewels.length; i++) {
-        for (let j=0; j<stones.length; j++) {
-            if (jewels[i] == stones[j]) {
-                count++;
-            }
+        jewelSet.add(jewels[i]);
+    }
+
+    let count = 0;
+    for (let i=0; i < stones.length; i++) {
+        if (jewelSet.has(stones[i])) {
+            count++;
         }
     }
 
