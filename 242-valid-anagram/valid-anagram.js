2b4666cb5fb1ee4004be6b9b75cd1bd046eccbe5
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-10-09 20:33:38 +0500

    Time: 6 ms (93.05%) | Memory: 55.5 MB (89.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    // One liner approach
+    // return s.split("").sort().join("") === t.split("").sort().join("");
+
+    if (s.length !== t.length) return false;
+
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        if (!map[s[i]]) {
+            map[s[i]] = 1;
+        } else {
+            map[s[i]]++;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (!map[t[i]] || map[t[i]] < 0) {
+            return false;
+        } else {
+            map[t[i]]--;
+        }   
+    }
+
+    return true;
+};
