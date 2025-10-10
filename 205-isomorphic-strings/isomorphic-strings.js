e1506749f2fecd58ca504360d8e59bf3782a960b
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-10-10 16:14:32 +0500

    Time: 8 ms (33.24%) | Memory: 55.1 MB (62.22%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -8,12 +8,13 @@ var isIsomorphic = function(s, t) {
     let mapTtoS = {};
 
     for (let i=0; i<s.length; i++) {
+        // If not there in the maps, store it.
         if (!mapStoT[s[i]] && !mapTtoS[t[i]]) {
             mapStoT[s[i]] = t[i];
             mapTtoS[t[i]] = s[i];
-        } else if (mapStoT[s[i]] !== t[i]) {
-            return false;
-        } else if (mapTtoS[t[i]] !== s[i]) {
+        } 
+        // If a character is already in the map, check if it's mapping to the same character or not;
+        else if (mapStoT[s[i]] !== t[i] || mapTtoS[t[i]] !== s[i]) {
             return false;
         }
     }
