98889311b2e386945e52dd4ec18c2194b90afac1
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-04 15:46:14 +0500

    Time: 85 ms (5.10%) | Memory: 66 MB (5.44%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = [...nums1, ...nums2];
+    arr.sort((a, b) => a - b);
+   
+    let median = Math.floor(arr.length / 2);
+    let result = 0;
+
+    if (arr.length % 2 !== 0) {
+    result = arr[median];
+    } else {
+        result = ((arr[median] + arr[median - 1]) / 2)
+    }
+
+    console.log("Median: ", median);
+    console.log("Result: ", result);
+    console.log("Arr: ", arr);
+
+    return result;
+};
