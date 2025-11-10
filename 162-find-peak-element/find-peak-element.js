f689cf1dda7c83d04d038541ae8121869fdde58b
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-10 19:13:58 +0500

    Time: 0 ms (100.00%) | Memory: 55.2 MB (11.52%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left)/2);
+
+        if (nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
+};
