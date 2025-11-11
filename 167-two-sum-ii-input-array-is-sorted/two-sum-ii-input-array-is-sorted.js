7f6757b0c05bb499720546d08d0f54e8a0912843
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-11 18:55:22 +0500

    Time: 4 ms (20.45%) | Memory: 55.9 MB (22.68%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i = 0; j = numbers.length - 1;
+
+    while(i <= j) {
+        if (numbers[i] + numbers[j] === target) {
+            // I need to work with 1 base indexing (so, adding 1 to cover that condition)
+            return [i + 1, j + 1];
+        } else if (numbers[i] + numbers[j] > target) {
+            j--;
+        } else if (numbers[i] + numbers[j] < target) {
+            i++;
+        }
+    } 
+};
