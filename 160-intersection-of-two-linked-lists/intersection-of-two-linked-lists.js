33f2a0ee579336dc13889b061457a12434f92df0
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-12 16:29:59 +0500

    Time: 68 ms (31.22%) | Memory: 65.5 MB (31.14%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -13,19 +13,63 @@
  */
 var getIntersectionNode = function(headA, headB) {
     // Create set from headB 
-    let store = new Set();
+    // let store = new Set();
 
-    while (headB) {
-        store.add(headB);
-        headB = headB.next;
+    // while (headB) {
+    //     store.add(headB);
+    //     headB = headB.next;
+    // }
+
+    // // Check each element of headA in the set
+    // while (headA) {
+    //     if (store.has(headA)) {
+    //         return headA;
+    //     }
+    //     headA  = headA.next;
+    // }
+
+    // return null;
+
+
+
+    //  Two pointer approach
+    let A = headA;
+    let B = headB;
+    let n = m = 0;
+
+    while(A) {
+        n++;
+        A = A.next;
     }
 
-    // Check each element of headA in the set
-    while (headA) {
-        if (store.has(headA)) {
-            return headA;
+    while(B) {
+        m++;
+        B = B.next;
+    }
+
+    console.log("N: ", n, " M: ", m);
+
+    if (n > m) {
+        let difference = n - m;
+        let i = 0;
+        while(i < difference) {
+            headA = headA.next;
+            i++;
         }
-        headA  = headA.next;
+    } else  if (n < m){
+        let difference = m - n;
+        let i=0;
+        while(i < difference) {
+            i++;
+            headB = headB.next;
+        }
+    }
+
+    while(headA || headB) {
+        if (headA == headB) return headA;
+
+        headA = headA.next;
+        headB = headB.next;
     }
 
     return null;
