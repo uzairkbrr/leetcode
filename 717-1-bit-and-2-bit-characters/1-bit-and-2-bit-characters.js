ed6cb341b834991e22ed40c72e01d68fb514ef2e
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-18 11:48:27 +0500

    Time: 0 ms (100.00%) | Memory: 54.4 MB (44.14%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function(bits) {
+    let i = 0;
+
+    while (i < bits.length - 1) {
+        if (bits[i] === 1) i += 2;
+        else i += 1;
+    }
+
+    return i === bits.length - 1;
+};
