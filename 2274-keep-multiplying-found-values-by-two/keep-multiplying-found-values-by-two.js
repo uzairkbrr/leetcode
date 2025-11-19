7d512c20b800c8791f34b235b619ee8724038508
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-19 12:00:16 +0500

    Time: 1 ms (50.56%) | Memory: 55.5 MB (36.11%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
@@ -6,11 +6,7 @@
 var findFinalValue = function(nums, original) {
     let set = new Set(nums);
 
-    for (let i=0; i<nums.length; i++) {
-        if (set.has(original)) {
-            original *= 2;
-        }
-    }
+    while(set.has(original)) original *= 2;
 
     return original;
 };
