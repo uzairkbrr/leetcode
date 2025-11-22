fbfbc813c7b9f532e699996b4c02e5fbb168b39d
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-22 17:03:10 +0500

    Time: 0 ms (100.00%) | Memory: 54.6 MB (25.08%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function(num1, num2) {
+    let operations = 0;
+
+    while (num1 != 0 && num2 != 0) {
+        if (num1 >= num2) {
+            num1 = num1 - num2;
+        } else {
+            num2 = num2 - num1;
+        }
+        operations++;
+    }
+
+    return operations;
+};
