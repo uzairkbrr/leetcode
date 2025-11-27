49f748a38ff7afd548a71f46de8c782b01913782
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2025-11-27 18:27:33 +0500

    Time: 50 ms (90.54%) | Memory: 87 MB (51.66%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -4,29 +4,21 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-    let dq = [];
-    let result = [];
-
-    for (let i = 0; i < nums.length; i++) {
-
-        // 1. Remove elements out of window
-        if (dq.length && dq[0] === i - k) {
-            dq.shift();
+    var queue = []
+    var result = []
+    
+    for(let i =0;i<nums.length;i++){
+        // console.log(queue.length , i, k,  i-k)
+        if(queue.length && queue[0] <= i - k){
+            queue.shift()
         }
-
-        // 2. Remove smaller elements (they are useless)
-        while (dq.length && nums[dq[dq.length - 1]] < nums[i]) {
-            dq.pop();
+        while(queue.length && nums[queue[queue.length-1]] <= nums[i]){
+            queue.pop()
         }
-
-        // 3. Push current element index
-        dq.push(i);
-
-        // 4. Starting from i >= k - 1, record max
-        if (i >= k - 1) {
-            result.push(nums[dq[0]]);
+        queue.push(i)
+        if(i >= k-1){
+            result.push(nums[queue[0]])
         }
     }
-
-    return result;
-};
+    return result
+  };
