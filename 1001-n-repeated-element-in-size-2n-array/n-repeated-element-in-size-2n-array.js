073beb4d23e6e7df66a0d91f71d2b8b45cd96f1e
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2026-01-03 03:39:34 +0500

    Time: 0 ms (100.00%) | Memory: 55.8 MB (98.31%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.js
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.js
@@ -2,14 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var repeatedNTimes = function(nums) {
-    let map = {};
+// var repeatedNTimes = function(nums) {
+//     let map = {};
 
-    for (let i=0; i<nums.length; i++) {
-        if (map[nums[i]]) {
-            return nums[i];
-        } else {
-            map[nums[i]] = true;
-        }
-    }   
+//     for (let i=0; i<nums.length; i++) {
+//         if (map[nums[i]]) {
+//             return nums[i];
+//         } else {
+//             map[nums[i]] = true;
+//         }
+//     }   
+// };
+
+var repeatedNTimes = function(nums) {
+    let seen = new Set();
+    for (let num of nums) {
+        if (seen.has(num)) return num;
+        seen.add(num);
+    }
 };
