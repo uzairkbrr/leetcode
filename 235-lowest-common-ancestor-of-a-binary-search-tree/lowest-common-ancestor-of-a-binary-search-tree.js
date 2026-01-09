85ebb35c012e7e14389cfc4f6ff9b9e72f904420
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2026-01-09 20:52:14 +0500

    Time: 67 ms (40.08%) | Memory: 65.8 MB (9.31%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -17,10 +17,9 @@ var lowestCommonAncestor = function(root, p, q) {
     if (p.val < root.val && q.val < root.val) {
         return lowestCommonAncestor(root.left, p, q);
     } 
-
-    if (p.val > root.val && q.val > root.val) {
+    else if (p.val > root.val && q.val > root.val) {
         return lowestCommonAncestor(root.right, p, q);
     }
-
-    return root;
+    else 
+        return root;
 };
