2085a999e1d0c9096a726c05db86c1c5f52a42ec
Uzair Ahmad <112511661+uzairkbrr@users.noreply.github.com>
2026-01-10 23:36:36 +0500

    Time: 1 ms (89.70%) | Memory: 55.5 MB (69.55%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    let first = -Infinity;
+    let second = -Infinity;
+    let third = -Infinity;
+
+    for (let num of nums) {
+        if (num === first || num === second || num === third) {
+            continue;
+        }
+
+        if (num > first) {
+            third = second;
+            second = first;
+            first = num;
+        } else if (num > second) {
+            third = second;
+            second = num;
+        } else if (num > third) {
+            third = num;
+        }
+    }
+
+    return third === -Infinity ? first : third;
+};
